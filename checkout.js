097ffe8667d16b3e853d4f63d886b9fb691c5b57
Contra <contra@maricopa.edu>
2015-03-26 12:47:26 -0700

    fix

--- checkout.js
+++ checkout.js
@@ -8,7 +8,7 @@ var dev = phidget.create({
   serial: '35193'
 });
 dev.on('touch', function(id){
-  if (!needsCheckout) {
+  if (!status.needsCheckout) {
     status.needsCheckout = true;
     console.log('needs checkout');
   }
